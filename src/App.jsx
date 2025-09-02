ef8eaa27318926c479b2908985c5800938719002
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-09-02 02:50:54 +0200

    [UPD] struttura base html

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 const articoliBorsa = [
-  "calzettoni",
-  "scarpe",
-  "pantaloncini",
-  "maglietta",
-  "accappatoio",
-  "shampoo",
-  "ricambio",
+  "Calzettoni",
+  "Scarpe",
+  "Pantaloncini",
+  "Maglietta",
+  "Accappatoio",
+  "Shampoo",
+  "Mutande",
+  "Calze",
 ];
 
 import { useState } from "react";
@@ -13,7 +14,19 @@ import { useState } from "react";
 function App() {
   return (
     <>
-      <h1>React Form</h1>
+      <h1>Articoli Borsa da Calcio</h1>
+      <div className=" my-5">
+        <div className="row">
+          <div className="col-12">
+            <h4>Lista Articoli</h4>
+          </div>
+          <div className="col-12">
+            <ul className="list-unstyled">
+              
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
