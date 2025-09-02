7d612ecb411c8bba820e8df0edb1cd9795094c04
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-09-02 02:46:33 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+const articoliBorsa = [
+  "calzettoni",
+  "scarpe",
+  "pantaloncini",
+  "maglietta",
+  "accappatoio",
+  "shampoo",
+  "ricambio",
+];
+
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1>React Form</h1>
+    </>
+  );
+}
+
+export default App;
